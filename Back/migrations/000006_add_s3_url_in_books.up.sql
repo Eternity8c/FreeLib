8c12140c0a42347735e30868355595c0c5510c4a
Eternity8c <morozkind6@gmail.com>
2026-06-17 15:34:42 +0300

    Добавил миграцию для добавления файлов книг

---
+++ Back/migrations/000006_add_s3_url_in_books.up.sql
@@ -0,0 +1,19 @@
+UPDATE freelib.books 
+SET s3_url = 'https://storage.yandexcloud.net/tes-freelib-server/1781625898_Толстой Лев. Анна Каренина - royallib.com.epub'
+WHERE title = 'Анна Каренина';
+
+UPDATE freelib.books 
+SET s3_url = 'https://storage.yandexcloud.net/tes-freelib-server/1781625898_Достоевский Федор. ИДИОТ - royallib.com.epub'
+WHERE title = 'Идиот';
+
+UPDATE freelib.books
+SET s3_url = 'https://storage.yandexcloud.net/tes-freelib-server/1781625898_Чехов Антон. Палата № 6 - royallib.com.epub'
+WHERE title = 'Палата №6';
+
+UPDATE freelib.books 
+SET s3_url = 'https://storage.yandexcloud.net/tes-freelib-server/1781625898_Оруэлл Джордж. Скотный двор - royallib.com.epub'
+WHERE title = 'Скотный двор';
+
+UPDATE freelib.books 
+SET s3_url = 'https://storage.yandexcloud.net/tes-freelib-server/1781625898_Пушкин Александр. Медный всадник - royallib.com.epub'
+WHERE title = 'Медный всадник';
